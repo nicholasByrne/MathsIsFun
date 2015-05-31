713a1eccaaf7be732e98787f6e9ca5f0b4c72ef7
nicholasByrne <n.j.byrne@hotmail.com>
2015-05-31 21:35:59 +1000

    Moved Vectors to MathLibrary (possibly screwed up original vectors by trying to rename it)

---
+++ MathLibrary/MathLibrary/Vector2.cpp
@@ -0,0 +1,143 @@
+#include "Vector2.h"
+
+
+Vector2::Vector2()
+{
+
+}
+
+
+Vector2::Vector2(float a_x, float a_y)
+{
+	x = a_x;
+	y = a_y;
+}
+
+
+Vector2::~Vector2()
+{
+}
+
+
+bool Vector2::operator==(Vector2 &other)
+{
+	if (x == other.x && y == other.y)
+	{
+		return true;
+	}
+	else
+		return false;
+}
+
+
+void Vector2::operator=(Vector2 &other)
+{
+	x = other.x;
+	y = other.y;
+}
+
+
+Vector2 Vector2::operator+(Vector2 &other)
+{
+	Vector2 temp;
+	temp.x = x + other.x;
+	temp.y = y + other.y;
+	return temp;
+}
+
+
+void Vector2::operator+=(Vector2 &other)
+{
+	x += other.x;
+	y += other.y;
+}
+
+
+Vector2 Vector2::operator-(Vector2 &other)
+{
+	Vector2 temp;
+	temp.x = x - other.x;
+	temp.y = y - other.y;
+	return temp;
+}
+
+
+void Vector2::operator-=(Vector2 &other)
+{
+	x -= other.x;
+	y -= other.y;
+}
+
+
+Vector2 Vector2::operator*(float scalar)
+{
+	Vector2 temp;
+	temp.x = x * scalar;
+	temp.y = y * scalar;
+	return temp;
+}
+
+
+void Vector2::operator*=(float scalar)
+{
+	x = x * scalar;
+	y = y * scalar;
+}
+
+
+Vector2 Vector2::operator/(Vector2 &other)
+{
+	Vector2 temp;
+	temp.x = x / other.x;
+	temp.y = y / other.y;
+	return temp;
+}
+
+
+void Vector2::operator/=(Vector2 &other)
+{
+	x = x / other.x;
+	y = y / other.y;
+}
+
+
+float Vector2::Magnitude()
+{
+	return sqrt((x * x) + (y * y));
+}
+
+
+float Vector2::MagnitudeSqrd()
+{
+	return ((x * x) + (y * y));
+}
+
+
+void Vector2::NormaliseThis()
+{
+	float magnitude = Magnitude();
+	if (magnitude != 0)
+	{
+		x /= magnitude;
+		y /= magnitude;
+	}
+}
+
+
+Vector2 Vector2::Normalise()
+{
+	Vector2 temp;
+	float magnitude = Magnitude();
+	if (magnitude != 0)
+	{
+		temp.x = x / magnitude;
+		temp.y = y / magnitude;
+	}
+	return temp;
+}
+
+
+float Vector2::dot(Vector2 &other)
+{
+	return ((x * other.x) + (y * other.y));
+}
---
+++ MathLibrary/MathLibrary/Vector2.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <math.h>
+
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(float a_x, float a_y);
+	~Vector2();
+
+	float x, y;
+
+	bool operator==(Vector2 &other);
+	void operator=(Vector2 &other);
+
+	Vector2 operator+(Vector2 &other);
+	void operator+=(Vector2 &other);
+
+	Vector2 operator-(Vector2 &other);
+	void operator-=(Vector2 &other);
+
+	Vector2 operator*(float scalar);
+	void operator*=(float scalar);
+
+	Vector2 operator/(Vector2 &other);
+	void operator/=(Vector2 &other);
+
+	float Magnitude();
+	float MagnitudeSqrd();
+
+	void NormaliseThis(); //
+	Vector2 Normalise(); //returns normalised version
+
+	float dot(Vector2 &other);
+
+private:
+
+};
